2ca6297b91af2d17abb274f544ae3057c478b1ba
Daniel <danhwang88@gmail.com>
2016-06-24 12:57:35 -0400

    greengenes 13.8 85 percent mgdb

---
+++ R/gg13.8.85MgDb.R
@@ -0,0 +1,13 @@
+###############################################
+#'  MgDb-class object for greengenes version 13.8 85 OTU
+#'
+#' gg13.8OTU85MgDb is a MgDb-class object with
+#' sequence and taxonomic data for the greengenes
+#' database version 13.8OTU85, \url{http://greengenes.secondgenome.com}
+#'
+#' @name gg13.8OTU85MgDb
+#' @seealso \link[metagenomeFeatures]{MgDb-class}
+#' @examples
+#' gg13.8OTU85MgDb
+NULL
+###############################################
